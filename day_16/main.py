5949a8286cd4b40c8ab5740dc22133643e2616ca
Leo Lindqvist Kröhnert <leolikro01@gmail.com>
2026-05-28 11:30:32 +0200

    Coffe_machine

--- day_16/main.py
+++ day_16/main.py
@@ -2,7 +2,11 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-reporty = CoffeeMaker
+reporty = CoffeeMaker()
 
 reporty.report()
 print(reporty)
+
+left = CoffeeMaker(drink)
+left.is_resource_sufficient(drink)
+print(left)
