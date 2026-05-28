4617d8f753772aae1565971216510ec790127fb7
LeoLindqvist123 <leolikro01@gmail.com>
2026-05-28 21:47:21 +0200

    Class

---
+++ day_17/main.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self):
+        print("new user beign created")
+
+
+user_1 = User()
+user_1.id = "001"
+user_1.username = "Leo"
+
+print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "Leona"
