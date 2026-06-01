f44976334fcb841e2acf635e14aa633320ffebe0
LeoLindqvist123 <leolikro01@gmail.com>
2026-06-01 13:16:38 +0200

    Working

--- day_17/quiz_brain.py
+++ day_17/quiz_brain.py
@@ -6,20 +6,17 @@ class QuizBrain:
         self.score = 0
 
     def still_has_questions(self):
-        if self.question_number < len(self.question_list):
-            return True
-        else:
-            False
-
+        return self.question_number < len(self.question_list)
+    
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        self.question_number =+ 1
-        user_answer = input(f"Q.{current_question}: {current_question.text} True/False: ")
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}. {current_question.text} True/False: ")  # ← här
         self.check_answer(user_answer, current_question.answer)
 
 
     def check_answer(self, user_answer, correct_answer):
-        if user_answer.lower == correct_answer.lower():
+        if user_answer.lower() == correct_answer.lower():
             self.score += 1
             print("You got it right!")
         else:
