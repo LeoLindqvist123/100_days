aa101560edd118daa698387384e9d228f224caca
LeoLindqvist123 <leolikro01@gmail.com>
2026-06-01 12:40:03 +0200

    Working

--- day_17/quiz_brain.py
+++ day_17/quiz_brain.py
@@ -1,8 +1,10 @@
 class QuizBrain:
+
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
     
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        input(f"Q.{current_question}: {current_question.text} True/False")
+        self.question_number =+ 1
+        input(f"Q.{current_question}: {current_question.text} True/False: ")
