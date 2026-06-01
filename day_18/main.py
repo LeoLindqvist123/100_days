6a71a95b516902873aa5e55f1d7714b319fda859
LeoLindqvist123 <leolikro01@gmail.com>
2026-06-01 13:39:51 +0200

    turtle square

--- day_18/main.py
+++ day_18/main.py
@@ -1,14 +1,13 @@
 from turtle import Turtle, Screen
 
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
+leona_the_turtle = Turtle()
+leona_the_turtle.shape("turtle")
+leona_the_turtle.color("blue")
 
-
-
-
-
-
+for i in range(8):
+    leona_the_turtle.forward(100)
+    leona_the_turtle.right(90)
 
 screen = Screen()
 screen.exitonclick()
