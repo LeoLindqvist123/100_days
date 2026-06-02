903978354d0a8405873dd65a2f1e2a27e4d8b7d7
LeoLindqvist123 <leolikro01@gmail.com>
2026-06-02 11:07:48 +0200

    Heart shape

--- day_18/main.py
+++ day_18/main.py
@@ -5,11 +5,39 @@ leona_the_turtle = Turtle()
 leona_the_turtle.shape("turtle")
 leona_the_turtle.color("blue")
 
-for i in range(20):
-    leona_the_turtle.forward(10)
-    leona_the_turtle.penup()
-    leona_the_turtle.forward(10)
-    leona_the_turtle.pendown()
+def curve():
+    for i in range(200):
+
+        leona_the_turtle.right(1)
+        leona_the_turtle.forward(1)
+
+
+def heart():
+
+    leona_the_turtle.fillcolor('red')
+    leona_the_turtle.begin_fill()
+    leona_the_turtle.left(140)
+    leona_the_turtle.forward(113)
+    curve()
+    leona_the_turtle.left(120)
+    curve()
+    leona_the_turtle.forward(112)
+    leona_the_turtle.end_fill()
+
+# Defining method to write text
+def txt():
+    
+    leona_the_turtle.up()
+    leona_the_turtle.setpos(-68, 95)
+    leona_the_turtle.down()
+    leona_the_turtle.color('lightgreen')
+
+    leona_the_turtle.write("Leona", font=("Verdana", 12, "bold"))
+
+heart()
+txt()
+leona_the_turtle.ht()
+ 
 
 
 screen = Screen()
