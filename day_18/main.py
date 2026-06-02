9994e442bd3a91d6a40899d4068da2f585e7b592
LeoLindqvist123 <leolikro01@gmail.com>
2026-06-02 11:11:35 +0200

    Heart shape

--- day_18/main.py
+++ day_18/main.py
@@ -1,44 +1,14 @@
-from turtle import Turtle, Screen
+import turtle as t
+
+tim = t.Turtle()
 
 
-leona_the_turtle = Turtle()
-leona_the_turtle.shape("turtle")
-leona_the_turtle.color("blue")
 
-def curve():
-    for i in range(200):
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for i in range(num_sides):
+       tim.forward(100)
+       tim.right(angle)
 
-        leona_the_turtle.right(1)
-        leona_the_turtle.forward(1)
-
-
-def heart():
-
-    leona_the_turtle.fillcolor('red')
-    leona_the_turtle.begin_fill()
-    leona_the_turtle.left(140)
-    leona_the_turtle.forward(113)
-    curve()
-    leona_the_turtle.left(120)
-    curve()
-    leona_the_turtle.forward(112)
-    leona_the_turtle.end_fill()
-
-# Defining method to write text
-def txt():
-    
-    leona_the_turtle.up()
-    leona_the_turtle.setpos(-68, 95)
-    leona_the_turtle.down()
-    leona_the_turtle.color('lightgreen')
-
-    leona_the_turtle.write("Leona", font=("Verdana", 12, "bold"))
-
-heart()
-txt()
-leona_the_turtle.ht()
- 
-
-
-screen = Screen()
-screen.exitonclick()
+for  shape_side_n in range(3,11):
+    draw_shape(shape_side_n)
