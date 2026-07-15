f8199a1a53a0d4310e606336faf5497aeb69c41c
Leo Lindqvist Kröhnert <leolikro01@gmail.com>
2026-07-15 16:08:40 +0200

    keep learning

--- day_18/learning.py
+++ day_18/learning.py
@@ -2,13 +2,22 @@ import turtle as t
 import random
 
 tim = t.Turtle()
+t.colormode(255)
 
-colours = ["blue", "green", "yellow", "orange", "red", "purple"]
+def random_col():
+   r = random.randint(0, 255)
+   g = random.randint(0, 255)
+   b = random.randint(0, 255)
+   random_col = (r, g, b)
+   return random_col
+
+
+
 directions = [0, 90, 180, 270]
 tim.pensize(5)
 tim.speed("fastest")
 
 for i in range(200):
-   tim.color(random.choice(colours))
+   tim.color(random_col())
    tim.forward(30)
    tim.setheading(random.choice(directions))
