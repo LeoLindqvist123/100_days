733abc1f22b729ba4e63dc28d0a4e2af8b5f8007
leolindqvist123 <leolikro01@gmail.com>
2026-08-05 11:41:19 +0200

    snake game

--- day_20/snake_game.py
+++ day_20/snake_game.py
@@ -1,10 +1,19 @@
 from turtle import Turtle, Screen
 
+all_squares = []
+x_posistions = [-20, -40, 0]
+
+for square_index in range(0, 3):
+    new_square = Turtle(shape="square")
+    new_square.color("white")
+    new_square.penup()
+    new_square.goto(x=x_posistions[square_index], y=0)
+    all_squares.append(new_square)
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Leos game")
-
+screen.title("Leos Game")
 
 
 
