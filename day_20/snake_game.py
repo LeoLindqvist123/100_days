08b726e2f9e61a34a6f5414146f8ddba8e6d7a8b
leolindqvist123 <leolikro01@gmail.com>
2026-08-05 12:29:34 +0200

    snake

--- day_20/snake_game.py
+++ day_20/snake_game.py
@@ -25,9 +25,12 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(start= 2, stoper= 0, step= -1):
-        segments[seg_num - 1]
-        segments[seg_num].goto()
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+
         
 
 
