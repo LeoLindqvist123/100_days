bcf3017479f6fb837194366e5dc96bbeba68d457
leolindqvist123 <leolikro01@gmail.com>
2026-08-06 14:30:24 +0200

    game over

--- day_20/main.py
+++ day_20/main.py
@@ -30,12 +30,19 @@ while game_is_on:
     #detect collison food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     #detect collison wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    #detect collison tail
+    for segment in snake.segments:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
