33ae363e8874c0f20a3e71ef8350b430ddac1de2
leolindqvist123 <leolikro01@gmail.com>
2026-08-06 13:48:38 +0200

    food

--- day_20/main.py
+++ day_20/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
+from food import Food
 
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.title("Leos Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,8 +23,9 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    #detect collison food
 
 
 screen.exitonclick()
