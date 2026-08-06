6b71277c61f317aecc16ecd23e768f4b79de4275
leolindqvist123 <leolikro01@gmail.com>
2026-08-06 12:14:14 +0200

    snake

--- day_20/snake.py
+++ day_20/snake.py
@@ -24,22 +24,22 @@ class Snake:
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num].ycor()
+            new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
         if self.head.heading() != DOWN:
-           self.head.setheading(UP)
+            self.head.setheading(UP)
 
     def down(self):
         if self.head.heading() != UP:
-           self.head.setheading(DOWN)
+            self.head.setheading(DOWN)
 
     def left(self):
         if self.head.heading() != RIGHT:
-           self.head.setheading(LEFT)
+            self.head.setheading(LEFT)
 
     def right(self):
         if self.head.heading() != LEFT:
-           self.head.setheading(RIGHT)
+            self.head.setheading(RIGHT)
