9e62e7f59a6b75d8ead8400803dd21ebf233b973
leolindqvist123 <leolikro01@gmail.com>
2026-08-07 17:13:28 +0200

    neraly done

--- day_22/main.py
+++ day_22/main.py
@@ -18,12 +18,12 @@ scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "W")
-screen.onkey(l_paddle.go_down, "S")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.05)
     screen.update()
     ball.move()
 
