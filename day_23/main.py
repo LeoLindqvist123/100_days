96358b9e79105e37a8564b5de7d2d3f46d68b896
leolindqvist123 <leolikro01@gmail.com>
2026-08-09 22:30:54 +0200

    creating new game

--- day_23/main.py
+++ day_23/main.py
@@ -8,6 +8,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+carmanager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -16,6 +17,9 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    carmanager.create_cars()
+    carmanager.move_cars()
 
 
 screen.exitonclick()
