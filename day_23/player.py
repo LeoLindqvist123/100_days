998553e87e925e3856b7a9c4cad5a3ba0ce3cd1e
leolindqvist123 <leolikro01@gmail.com>
2026-08-09 20:23:46 +0200

    creating new game

--- day_23/player.py
+++ day_23/player.py
@@ -15,5 +15,4 @@ class Player(Turtle):
         self.setheading(90)
 
     def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+        self.forward(MOVE_DISTANCE)
