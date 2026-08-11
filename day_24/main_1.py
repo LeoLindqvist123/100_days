d4d1793c957790753e78510b18070627c0bfd7b8
leolindqvist123 <leolikro01@gmail.com>
2026-08-11 11:56:08 +0200

    added highscore

--- day_24/main_1.py
+++ day_24/main_1.py
@@ -1,3 +1,11 @@
-with open("my_file.txt") as f:
+'''with open("my_file.txt") as f:
     contents = f.read()
     print(contents)
+'''
+# mode = "a" for append and "w" for write
+
+'''with open("my_file.txt", mode="a") as file:
+    file.write("\nNew text")'''
+
+with open("new_file.txt", mode="w") as file:
+    file.write("Tjenare mannen")
