faf0b6e961d182417e6c200a8780ba1797df1b55
leolindqvist123 <leolikro01@gmail.com>
2026-08-12 10:09:33 +0200

    big csv data

--- day_25/main.py
+++ day_25/main.py
@@ -10,9 +10,9 @@ with open("weather_data.csv") as data_file:
 
 '''
 
-import pandas
+#import pandas
 
-data = pandas.read_csv("weather_data.csv")
+#data = pandas.read_csv("weather_data.csv")
 
 #temp_list = data["temp"].to_list()
 #print(len(temp_list))
@@ -21,4 +21,16 @@ data = pandas.read_csv("weather_data.csv")
 
 #print(data[data.day == "Monday"])
 
-print(data[data.temp == data.temp.max()])
+# print(data[data.temp == data.temp.max()])
+
+#monday = data[data.day == "Monday"]
+#monday_temp = monday.temp[0]
+#monday_temp_f = monday_temp * 9/5 + 32
+#print(monday_temp_f)
+
+import pandas
+
+data = pandas.read_csv("squirrels.csv")
+
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+print(grey_squirrels_count)
