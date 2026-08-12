4a274d4a86718062fb39b1d10d00c5cfb02116ca
leolindqvist123 <leolikro01@gmail.com>
2026-08-12 10:13:05 +0200

    big csv data

--- day_25/main.py
+++ day_25/main.py
@@ -38,3 +38,11 @@ black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
 print(grey_squirrels_count)
 print(red_squirrels_count)
 print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("Squirrel_count.csv")
