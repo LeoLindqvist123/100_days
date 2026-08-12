4409826196e9a61b9abea63d1d921117c57a1066
leolindqvist123 <leolikro01@gmail.com>
2026-08-12 12:28:19 +0200

    us state game

--- day_25/us_states/main.py
+++ day_25/us_states/main.py
@@ -1,23 +1,25 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
+correct = []
 
-turtle.onscreenclick(get_mouse_click_coor)
+all_state = data.state.to_list()
 
-turtle.mainloop()
+answer_state = turtle.textinput(title="Guess the State", prompt="What's another state's name?").title()
 
-
-
-
-
-
-screen.exitonclick()
+if answer_state in all_state:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    t.goto(state_data.x, state_data.y)
+    correct.append(answer_state)
+    correct += "1"
